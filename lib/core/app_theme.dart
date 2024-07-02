93e19c98ec02f5f453448cebab00eb3296ff30f7
Kirill Zverev <kirill.zverev@whitetigersoft.ru>
2024-07-02 09:50:01 +0300

    feat: assign common colors

---
+++ lib/core/app_theme.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const pinkMain = Color(0xFFF3E5F5);
+  static const pinkXxl = Color(0xFFEA80FC);
+  static const pinkXl = Color(0xFFBA68C8);
+  static const pinkL = Color(0xFFE1BEE7);
+  static const purple = Color(0xFF6A1B9A);
+  static const blackL = Color.fromRGBO(47, 47, 47, 1);
+  static const blackXl = Color.fromRGBO(28, 28, 28, 1);
+}
