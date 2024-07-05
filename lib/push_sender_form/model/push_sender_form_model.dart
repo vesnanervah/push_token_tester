605a7b00d5486607e9bcd9e932eda8ee416a2367
Kirill Zverev <kirill.zverev@whitetigersoft.ru>
2024-07-05 14:33:28 +0300

    feat: add status changes in push_sender_form

--- lib/push_sender_form/model/push_sender_form_model.dart
+++ lib/push_sender_form/model/push_sender_form_model.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:push_by_token_tester/core/model/base_form_page_model.dart';
+import 'package:push_by_token_tester/core/model/entities/form_status.dart';
 
 class PushSenderFormModel extends BaseFormPageModel {
   PushSenderFormModel({required super.appModel, required super.status});
@@ -17,13 +18,22 @@ class PushSenderFormModel extends BaseFormPageModel {
 
   @override
   Future<void> submitForm() async {
-    final result = await appModel.api.sendPush(
-      title: titleController.text,
-      text: textPushController.text,
-      body: bodyController.text,
-    );
-    if (!result) {
+    formStatusNotifier.value = FormStatus.loading;
+    try {
+      final result = await appModel.api.sendPush(
+        title: titleController.text,
+        text: textPushController.text,
+        body: bodyController.text,
+      );
+      if (!result) {
+        errorMsg = 'Что-то пошло не так';
+        formStatusNotifier.value = FormStatus.rejected;
+      } else {
+        formStatusNotifier.value = FormStatus.successful;
+      }
+    } catch (_) {
       errorMsg = 'Что-то пошло не так';
+      formStatusNotifier.value = FormStatus.rejected;
     }
   }
 }
