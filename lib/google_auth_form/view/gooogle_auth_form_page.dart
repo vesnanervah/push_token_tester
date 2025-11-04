d3d04f374d235d90eab7cd1412bbe9130d16af6b
Kirill Zverev <vesnanervakh@gmail.com>
2025-11-04 19:34:11 +0300

    refactor: replace credentials json input placeholder into separate field

--- lib/google_auth_form/view/gooogle_auth_form_page.dart
+++ lib/google_auth_form/view/gooogle_auth_form_page.dart
@@ -25,10 +25,7 @@ class _GoogleAuthFormPageState
     minLines: 12,
     controller: model.jsonTextController,
     maxLines: 14,
-    decoration: const InputDecoration(
-      hintText:
-          '{           "type": "service_account",           "project_id": "app-id",           "private_key_id": "key-id",           "private_key": "-----BEGIN PRIVATE KEY-----PRIVATE KEY-----END PRIVATE KEY-----",           "client_email": "clientemail.gserviceaccount.com",           "client_id": "111111111111111",           "auth_uri": "https://accounts.google.com/o/oauth2/auth",           "token_uri": "https://oauth2.googleapis.com/token",           "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",           "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebaseclientx509url",           "universe_domain": "googleapis.com"         }',
-    ),
+    decoration: const InputDecoration(hintText: _credentialsJsonPlaceholer),
     validator: (value) =>
         (value?.isEmpty ?? true) ? 'Не должно быть пустым' : null,
   );
@@ -57,3 +54,17 @@ class _GoogleAuthFormPageState
     ),
   );
 }
+
+const _credentialsJsonPlaceholer = '''
+    {
+      "type": "service_account",
+      "project_id": "app-id",          
+      "private_key_id": "key-id",          
+      "private_key": "-----BEGIN PRIVATE KEY-----PRIVATE KEY-----END PRIVATE KEY-----",          
+      "client_email": "clientemail.gserviceaccount.com",          
+      "client_id": "111111111111111",          
+      "auth_uri": "https://accounts.google.com/o/oauth2/auth",          
+      "token_uri": "https://oauth2.googleapis.com/token",          
+      "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",          "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509firebaseclientx509url",          
+      "universe_domain": "googleapis.com"        }
+    ''';
